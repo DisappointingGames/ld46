e4d137e5b6504f8b1d15200e00d5912ea8114378
Robbert Jongeling <RobbertJongeling@users.noreply.github.com>
2020-04-18 12:32:07 +0200

    some initial game logic

---
+++ src/CellType.ts
@@ -0,0 +1,6 @@
+export enum CellType {
+    Empty = 0,
+    Server = 1,
+    BrokenServer = 2,
+    PowerUp = 3,
+}
---
+++ src/World.ts
@@ -0,0 +1,49 @@
+import { Cell } from "./Cell";
+import { CellType } from "./CellType";
+
+export class World {
+
+    private scene: Phaser.Scene;
+    private width: integer;
+    private height: integer;
+
+    constructor(scene: Phaser.Scene, width: number, height: number) {
+        this.scene = scene;
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * initialize world of default size with servers and walk space
+     */
+    initialize() {
+
+    }
+
+    /**
+     * moving a wall. Idea is that you can only move repaired servers
+     * if you disagree, just add brokenserver as an or in from.celltype
+     * @param from 
+     * @param to 
+     */
+    moveWall(from: Cell, to: Cell) {
+        if(from.celltype == CellType.Server && (to.celltype == CellType.Empty || to.celltype == CellType.PowerUp)) {
+            to.celltype = from.celltype;
+            from.celltype = CellType.Empty;            
+        }
+    }
+
+    /**
+     * generate emergency event at random location in world
+     */
+    createEmergency() {
+        
+    }
+
+    /**
+     * increase the size of the world
+     */
+    increaseSize() {
+
+    }
+}
