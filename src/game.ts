135266c39e839d91a7a6b7cdf482e38c2cafa808
David Vollmar <david@vollmar.nl>
2020-04-18 11:43:00 +0200

    more cleanup

--- src/game.ts
+++ src/game.ts
@@ -2,25 +2,16 @@ import 'phaser';
 
 export default class Demo extends Phaser.Scene
 {
-    constructor ()
-    {
+    constructor () {
         super('demo');
     }
 
-    preload ()
-    {
+    preload () {
         this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image('libs', 'assets/libs.png');        
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
+    create () {        
         this.add.image(400, 300, 'libs');
 
         const logo = this.add.image(400, 70, 'logo');
